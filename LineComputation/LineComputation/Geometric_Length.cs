11325ec834c2d579ac87a89ab18e27d126669c2d
anandsaikumar <anandsai298@gmail.com>
2023-03-06 19:59:48 +0530

    [sai] Add : Calculation of the Length

---
+++ LineComputation/LineComputation/Geometric_Length.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputation
+{
+    public class Geometric_Length
+    {
+        int x1, x2, y1, y2;
+        public Geometric_Length(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void Calculate()
+        {
+            double res = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length of the Line" + res);
+        }
+    }
+}
--- LineComputation/LineComputation/Program.cs
+++ LineComputation/LineComputation/Program.cs
@@ -6,7 +6,17 @@ namespace LineComputation
     {
         static void Main(String[] args)
         {
-
+            Console.WriteLine("welcome to Line Comparision : ");
+            Console.WriteLine("enter x1 value");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter x2 value");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y1 value");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y2 value");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Geometric_Length len = new Geometric_Length(x1, x2, y1, y2);
+            len.Calculate();
         }
     }
 
